983140256f175ffe1f4c1c6e5a5827683b751b27
juanignacioolmedo <olmedo.juanig@gmail.com>
2025-03-07 11:12:33 -0300

    Commit inicial

---
+++ config/settings/test.py
@@ -0,0 +1,38 @@
+"""
+With these settings, tests run faster.
+"""
+
+from .base import *  # noqa: F403
+from .base import TEMPLATES
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="Rkpd5VADO9f0HOuq2EQDzvRpvpOxxZ032VtDi5nOvqsuS5wNZioZK2WKuEr71k5k",
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
+# PASSWORDS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+# DEBUGGING FOR TEMPLATES
+# ------------------------------------------------------------------------------
+TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore[index]
+
+# MEDIA
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#media-url
+MEDIA_URL = "http://media.testserver/"
+# Your stuff...
+# ------------------------------------------------------------------------------
